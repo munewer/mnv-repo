3b4aef69af079a97e056aacf829b8e042bede661
Münevver Ak <munewerak@gmail.com>
2021-08-19 17:26:32 +0300

    add checkbox tests

---
+++ cypress-automation-framework/cypress/integration/webdriver-uni/chekcboxes.js
@@ -0,0 +1,17 @@
+/// <reference types="Cypress" />
+
+describe("Test Checkboxes via WebdriverUni", () => {
+
+    it("Check and validate checkboxes", () => {
+        cy.visit("http://www.webdriveruniversity.com/");
+        cy.get("#dropdown-checkboxes-radiobuttons").invoke("removeAttr", "target").click({ force: true });
+
+        cy.get('[value="option-1"]').as("option-1")
+        cy.get("@option-1").check().should("be.checked");
+
+        cy.get('[value="option-3"]').as("option-3")
+        cy.get("@option-3").should("be.checked");
+        cy.get("@option-3").uncheck().should("not.be.checked");
+    });
+
+})
