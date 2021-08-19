8c34b018f8826d355a5e014e687161fa10fdd85a
Münevver Ak <munewerak@gmail.com>
2021-08-19 17:54:21 +0300

    Update chekcboxes.js

--- cypress-automation-framework/cypress/integration/webdriver-uni/chekcboxes.js
+++ cypress-automation-framework/cypress/integration/webdriver-uni/chekcboxes.js
@@ -19,4 +19,10 @@ describe("Test Checkboxes via WebdriverUni", () => {
         cy.get("@option-3").uncheck().should("not.be.checked");
     });
 
+    it("Check and validate multiple checkboxes", () => {
+        cy.visit("http://www.webdriveruniversity.com/");
+        cy.get("#dropdown-checkboxes-radiobuttons").invoke("removeAttr", "target").click({ force: true });
+
+        cy.get('[type="checkbox"]').check(["option-1", "option-2", "option-3", "option-4"]).should("be.checked");
+    });
 })
