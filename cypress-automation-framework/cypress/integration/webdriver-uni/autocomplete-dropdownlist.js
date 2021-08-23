ef2cc3db86e4db6f7e2763d91185825d9451a9e9
Münevver Ak <munewerak@gmail.com>
2021-08-23 14:58:05 +0300

    Create autocomplete-dropdownlist.js

---
+++ cypress-automation-framework/cypress/integration/webdriver-uni/autocomplete-dropdownlist.js
@@ -0,0 +1,34 @@
+/// <reference types="Cypress" />
+
+describe("Verify autocomplete dropdownlist via WebdriverUni", () => {
+
+    it("Select specific products via autocomplete list", () => {
+        cy.visit("http://www.webdriveruniversity.com/");
+        cy.get("#autocomplete-textfield").invoke("removeAttr", "target").click({ force: true });
+
+        cy.get("#myInput").type("A")
+        cy.get("#myInputautocomplete-list>*").as("autolist")
+        cy.get("@autolist").each(($el, index, $list) => {
+            const prod = $el.text();
+            const productToSelect = "Almond";
+
+            if (prod === productToSelect) {
+                $el.trigger("click");
+                cy.get("#submit-button").click()
+                cy.url().should("contain", productToSelect);
+            }
+        }).then(() => {
+            cy.get("#myInput").type("B");
+            cy.get("@autolist").each(($el, index, $list) => {
+                const prod = $el.text();
+                const secondSelection = "Bagels";
+                if (prod === secondSelection) {
+                    $el.trigger("click");
+                    cy.get("#submit-button").click()
+                    cy.url().should("contain", secondSelection);
+                }
+            })
+
+        })
+    })
+})
