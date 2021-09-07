fe260512908cf70deae0504d05d4a562dd6c722f
Münevver Ak <munewerak@gmail.com>
2021-09-07 14:50:22 +0300

    Create data-tables.js

---
+++ cypress-automation-framework/cypress/integration/webdriver-uni/data-tables.js
@@ -0,0 +1,56 @@
+/// <reference types="Cypress" />
+describe("Handling data via Webdriveruni", () => {
+    beforeEach(() => {
+        cy.visit("http://webdriveruniversity.com/");
+        cy.get("#data-table").invoke("removeAttr", "target").click({ force: true });
+    })
+
+    it("Calculate total age of all users", () => {
+        var userDetails = [];
+        let numb = 0;
+        cy.get("#thumbnail-1 td").each(($el, index, $list) => {
+            userDetails[index] = $el.text();
+        }).then(() => {
+            var i;
+            for (i = 0; i < userDetails.length; i++) {
+                //To extract number value, use if statement
+                if (Number(userDetails[i])) {
+                    numb += Number(userDetails[i]);
+                }
+            }
+            cy.log("Total age : " + numb);
+            expect(numb).eq(322);
+        })
+    });
+
+    it("Calcaulate and assert age of a given user based on surname", () => {
+
+        cy.get("#thumbnail-1 td:nth-child(2)").each(($el, index, $list) => {
+            const surname = $el.text();
+            if (surname.includes("Woods")) {
+                cy.get("#thumbnail-1 td:nth-child(2)").eq(index).next().then((age) => {
+                    const userAge = age.text();
+                    expect(userAge).to.eq("80");
+                })
+            }
+        })
+    })
+
+
+    it("Calcaulate and assert age of users who has same lastname", () => {
+        let numb = 0;
+        cy.get("#thumbnail-1 td:nth-child(2)").each(($el, index, $list) => {
+            const surname = $el.text();
+            if (surname.includes("Jackson")) {
+                cy.get("#thumbnail-1 td:nth-child(2)").eq(index).next().then((age) => {
+                    let userAge = age.text();
+                    numb += Number(userAge)
+                })
+            }
+        }).then(() => {
+            cy.log("Total age value : " + numb);
+            expect(numb).to.eq(150);
+        })
+
+    })
+});
