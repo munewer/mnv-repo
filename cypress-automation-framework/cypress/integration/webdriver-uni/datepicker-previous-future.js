843471385ccb284f45bf6d06152eadde451b2922
Münevver Ak <munewerak@gmail.com>
2021-09-14 15:46:47 +0300

    Create a new datepicker test

---
+++ cypress-automation-framework/cypress/integration/webdriver-uni/datepicker-previous-future.js
@@ -0,0 +1,69 @@
+/// <reference types="Cypress" />
+describe("Test datepicker via webdriveruni", () => {
+
+    it("Select a date from datepicker", () => {
+        cy.visit("http://webdriveruniversity.com/");
+        cy.get("#datepicker").invoke("removeAttr", "target").click({ force: true });
+        cy.get("#datepicker").click();
+
+        let cDate = new Date();
+        cDate.setDate(cDate.getDate());
+        var cYear = cDate.getFullYear();
+        var cMonth = cDate.toLocaleDateString("default", { month: "long" });
+        cy.log("Current Year : " + cYear);
+        cy.log("Current Month : " + cMonth);
+
+        let date = new Date();
+        date.setDate(date.getDate() - 17)
+        var Year = date.getFullYear();
+        var Month = date.toLocaleString("default", { month: "long" });
+        var Day = date.getDate();
+        cy.log("Year to select :" + Year);
+        cy.log("Month to select :" + Month);
+        cy.log("Day to select : " + Day)
+
+        function selectMonthAndYear() {
+            cy.get(".datepicker-dropdown").find(".datepicker-switch").first().then(currentDate => {
+                if (!currentDate.text().includes(Year)) {
+                    cy.get(".next").first().click();
+                    selectMonthAndYear();
+                }
+            }).then(() => {
+                cy.get(".datepicker-dropdown").find(".datepicker-switch").first().then(currentDate => {
+                    if (!currentDate.text().includes(Month)) {
+                        cy.get(".next").first().click();
+                        selectMonthAndYear();
+                    }
+                })
+            })
+        }
+
+        function selectPreviousMonthAndYear() {
+            cy.get(".datepicker-dropdown").find(".datepicker-switch").first().then(currentDate => {
+                if (!currentDate.text().includes(Year)) {
+                    cy.get(".prev").first().click();
+                    selectPreviousMonthAndYear();
+                }
+            }).then(() => {
+                cy.get(".datepicker-dropdown").find(".datepicker-switch").first().then(currentDate => {
+                    if (!currentDate.text().includes(Month)) {
+                        cy.get(".prev").first().click();
+                        selectPreviousMonthAndYear();
+                    }
+                })
+            })
+        }
+
+        function selectDay() {
+            cy.get("[class='day']").contains(Day).click();
+        }
+
+        if (date < cDate) {
+            selectPreviousMonthAndYear();
+            selectDay();
+        } else {
+            selectMonthAndYear();
+            selectDay();
+        }
+    });
+});
